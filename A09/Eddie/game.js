8989a27056692a038288e15795854e4b84b1997e
IncandescentBulb <64438139+IncandescentBulb@users.noreply.github.com>
2021-03-28 17:31:34 -0400

    starting toy mod

--- A09/Eddie/game.js
+++ A09/Eddie/game.js
@@ -1,42 +1,11 @@
 /*
 game.js for Perlenspiel 3.3.x
-Last revision: 2020-03-24 (BM)
+Last revision: 2018-10-14 (BM)
 
-Perlenspiel is a scheme by Professor Moriarty (bmoriarty@wpi.edu).
-This version of Perlenspiel (3.3.x) is hosted at <https://ps3.perlenspiel.net>
-Perlenspiel is Copyright © 2009-20 Brian Moriarty.
-This file is part of the standard Perlenspiel 3.3.x devkit distribution.
-
-Perlenspiel is free software: you can redistribute it and/or modify
-it under the terms of the GNU Lesser General Public License as published
-by the Free Software Foundation, either version 3 of the License, or
-(at your option) any later version.
-
-Perlenspiel is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-GNU Lesser General Public License for more details.
-
-You may have received a copy of the GNU Lesser General Public License
-along with the Perlenspiel devkit. If not, see <http://www.gnu.org/licenses/>.
-*/
-
-/*
-This JavaScript file is a template for creating new Perlenspiel 3.3.x games.
-By default, all event-handling function templates are COMMENTED OUT (using block-comment syntax), and are therefore INACTIVE.
-Uncomment and add code to the event handlers required by your project.
-Any unused event-handling function templates can be safely deleted.
-Refer to the tutorials and documentation at <https://ps3.perlenspiel.net> for details.
-*/
-
-/*
-The following comment lines are for JSHint <https://jshint.com>, a tool for monitoring code quality.
-You may find them useful if your development environment is configured to support JSHint.
-If you don't use JSHint (or are using it with a configuration file), you can safely delete these lines.
-*/
-
-/* jshint browser : true, devel : true, esversion : 5, freeze : true */
+/* jshint browser : true, devel : true, esversion : 6, freeze : true */
 /* globals PS : true */
+
+"use strict"; // do not remove this directive!
 
 /*
 PS.init( system, options )
@@ -48,42 +17,25 @@ Any value returned is ignored.
 [options : Object] = A JavaScript object with optional data properties; see API documentation for details.
 */
 
-// UNCOMMENT the following code BLOCK to expose the PS.init() event handler:
-
-/*
-
 PS.init = function( system, options ) {
-	"use strict"; // Do not remove this directive!
 
-	// Uncomment the following code line
-	// to verify operation:
+	// Establish grid dimensions
+	
+	PS.gridSize( 7, 7 );
+	
+	// Set background color to Perlenspiel logo gray
+	
+	PS.gridColor( 0x303030 );
+	
+	// Change status line color and text
 
-	// PS.debug( "PS.init() called\n" );
+	PS.statusColor( PS.COLOR_WHITE );
+	PS.statusText( "Touch any bead" );
+	
+	// Preload click sound
 
-	// This function should normally begin
-	// with a call to PS.gridSize( x, y )
-	// where x and y are the desired initial
-	// dimensions of the grid.
-	// Call PS.gridSize() FIRST to avoid problems!
-	// The sample call below sets the grid to the
-	// default dimensions (8 x 8).
-	// Uncomment the following code line and change
-	// the x and y parameters as needed.
-
-	// PS.gridSize( 8, 8 );
-
-	// This is also a good place to display
-	// your game title or a welcome message
-	// in the status line above the grid.
-	// Uncomment the following code line and
-	// change the string parameter as needed.
-
-	// PS.statusText( "Game" );
-
-	// Add any other initialization code you need here.
+	PS.audioLoad( "fx_click" );
 };
-
-*/
 
 /*
 PS.touch ( x, y, data, options )
@@ -95,23 +47,36 @@ This function doesn't have to do anything. Any value returned is ignored.
 [options : Object] = A JavaScript object with optional data properties; see API documentation for details.
 */
 
-// UNCOMMENT the following code BLOCK to expose the PS.touch() event handler:
-
-/*
-
 PS.touch = function( x, y, data, options ) {
-	"use strict"; // Do not remove this directive!
+	// Toggle color of touched bead from white to black and back again
+	// NOTE: The default value of a bead's [data] is 0, which happens to be equal to PS.COLOR_BLACK
 
-	// Uncomment the following code line
-	// to inspect x/y parameters:
+	PS.color( x, y, data ); // set color to current value of data
+	
+	// Decide what the next color should be.
+	// If the current value was black, change it to white.
+	// Otherwise change it to black.
 
-	// PS.debug( "PS.touch() @ " + x + ", " + y + "\n" );
+	let next; // variable to save next color
 
-	// Add code here for mouse clicks/touches
-	// over a bead.
+	if ( data === PS.COLOR_BLACK ) {
+		next = PS.COLOR_WHITE;
+	}
+	else {
+		next = PS.COLOR_BLACK;
+	}
+
+	// NOTE: The above statement could be expressed more succinctly using JavaScript's ternary operator:
+	// let next = ( data === PS.COLOR_BLACK ) ? PS.COLOR_WHITE : PS.COLOR_BLACK;
+	
+	// Remember the newly-changed color by storing it in the bead's data.
+	
+	PS.data( x, y, next );
+
+	// Play click sound.
+
+	PS.audioPlay( "fx_click" );
 };
-
-*/
 
 /*
 PS.release ( x, y, data, options )
